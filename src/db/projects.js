c53b0f1a7ba24b1771fcea2273f2055704b03f93
Sergio Torres <sergio.lo.torres@gmail.com>
2021-07-25 20:03:13 -0500

    fixed a minor typo on project db

--- src/db/projects.js
+++ src/db/projects.js
@@ -1,49 +1,49 @@
 
 const  ProjectData = [
     {
-        "id": 0,
+        "id": 1,
         "title":"Lets-Brew",
         "image":require("../../src/assets/screenshots/Lets-Brew.png").default,
         "url":"https://lets-brew-group12.herokuapp.com/",
         "repo":"https://github.com/MLemke24/Lets-Brew"
     },
     {
-        "id": 1,
+        "id": 2,
         "title":"Budget-Tracker",
         "image":require("../../src/assets/screenshots/budget-tracker.png").default,
         "url":"https://sergiolomans-budget-tracker.herokuapp.com/",
         "repo":"https://github.com/Sergioloman/Sergioloman.19"
     },
     {
-        "id": 2,
+        "id": 3,
         "title":"Go-Out-App",
         "image":require("../../src/assets/screenshots/Go-out-app.png").default,
         "url":"https://sergioloman.github.io/Should-you-go-out/",
         "repo":"https://github.com/Sergioloman/Should-you-go-out"
     },   
     {
-        "id": 3,
+        "id": 4,
         "title":"Note-Taker",
         "image":require("../../src/assets/screenshots/note-taker.PNG").default,
         "url":"https://note-taker-by-sergio.herokuapp.com/",
         "repo":"https://github.com/Sergioloman/Sergioloman.11"
     },
     {
-        "id": 4,
+        "id": 5,
         "title":"Karp! e-Diem",
         "image":require("../../src/assets/screenshots/karpediem.PNG").default,
         "url":"https://karp-ediem.herokuapp.com/",
         "repo":"https://github.com/Sergioloman/FullStack.Team3"
     },
     {
-        "id": 5,
+        "id": 6,
         "title":"Javascript Quiz",
         "image":require("../../src/assets/screenshots/jsquiz.png").default,
         "url":"https://sergioloman.github.io/Sergioloman04.github.io/",
         "repo":"https://github.com/Sergioloman/Sergioloman04.github.io"
     },
     {
-        "id": 6,
+        "id": 7,
         "title":"Weather Dashboard",
         "image":require("../../src/assets/screenshots/weatherdash.png").default,
         "url":"https://sergioloman.github.io/Sergioloman06.github.io/",
