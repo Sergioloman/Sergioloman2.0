cf07c1e8cfa8118779c95bc1eef5530fc8e62f8a
Sergio Torres <sergio.lo.torres@gmail.com>
2021-09-09 18:01:11 -0500

    list of competencies added

--- src/Pages/Resume/Resume.js
+++ src/Pages/Resume/Resume.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import CV from '../../assets/Resume/ResumeScreenshot.PNG';
 import './resume.css'
+import skills from '../../db/skills';
 
 function resume(){
     return(
         <section className='resume'>
-            <h3>Download</h3>
+            <h3>Ask me anything about : </h3>
+            <br/>
+            <p>{skills.join(`    ||    `)}</p>
+            <br/>    
+            <h3>Download me!</h3>
             <a className="text-link" href="../../assets/Resume/Sergio_Torres_Resume_2.5.pdf" target="_blank" download>
             <img className="resume-shot" src={CV} alt='resume screenshot'></img>
             </a>
---
+++ src/db/skills.js
@@ -0,0 +1,22 @@
+const skills = [
+    "CSS",
+    "HTML",
+    "JavaScript",
+    "Bootstrap",
+    "SQL",
+    "JQuery",
+    "Handlebars",
+    "RESTful APIs",
+    "MySQL",
+    "NoSql",
+    "OOP",
+    "MVC",
+    "PWA's",
+    "MERN Stack",
+    "MongoDB",
+    "Express",
+    "React",
+    "Node",
+]
+
+export default skills;
