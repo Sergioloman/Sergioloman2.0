6f9ddec8705e5d275b61c63c50861ef209c223c3
Sergio Torres <sergio.lo.torres@gmail.com>
2021-09-09 18:32:30 -0500

    changed href of title

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -3,7 +3,7 @@ import React from 'react';
 function Header(){
     return(
     <div id="title">
-        <a href="/">
+        <a href="/Sergioloman2.0/">
             <h1>Sergio Torres</h1>
         </a>
         <h2>Full Stack Web Developer for hire <span></span></h2>
